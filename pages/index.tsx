0aae8881d2516e4a0b474e60221ff776884ef2ee
jel180000 <80798802+jel180000@users.noreply.github.com>
2022-08-18 03:25:48 -0500

    Added "Item-Name" to form, form list, and item interfaces

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import React, {Component} from 'react'
 const Home = () => {
 
   interface FormData {
+    name: string,
     fireRate: string,
     damage: string,
     healthPoints: string,
@@ -14,6 +15,7 @@ const Home = () => {
   }
 
   interface WpnData {
+    n: string
     fr: number,
     d: number,
     hp: number,
@@ -22,7 +24,7 @@ const Home = () => {
     TTE: number,
   }
   
-  const [form, setForm] = useState<FormData>({fireRate: '4',
+  const [form, setForm] = useState<FormData>({name:'Sample-Name', fireRate: '4',
     damage: '25',
     healthPoints: '100',
     damageReduction: '0',});
@@ -37,10 +39,10 @@ const Home = () => {
   var dFloat = parseFloat(form.damage)
   var frFloat = parseFloat(form.fireRate)
 
-  const [posts, savePosts] = useState<WpnData[]>([{fr:4, d:25,  hp:100,  dr:0, ATE:4,  TTE:1}])
+  const [posts, savePosts] = useState<WpnData[]>([{n: 'Sample-Name', fr:4, d:25,  hp:100,  dr:0, ATE:4,  TTE:1}])
 
   function SaveForm() {
-  savePosts(posts => [...posts, {fr:frFloat, d:dFloat,  hp:hpFloat,  dr:drFloat, ATE:AmmoToElim,  TTE:TimeToElim,}]);
+  savePosts(posts => [...posts, {n:form.name, fr:frFloat, d:dFloat,  hp:hpFloat,  dr:drFloat, ATE:AmmoToElim,  TTE:TimeToElim,}]);
   }
 
   AmmoToElim = ((hpFloat*(1+drFloat*0.01))/dFloat)
@@ -74,6 +76,17 @@ const Home = () => {
 
         <div className='flex min-w-screen items-center flex-col'>
           <form>
+            <p className='w-80 h-10 text-2xl mx-5 text-sky-300'>Item-Name</p>
+
+            <input 
+            className='w-80 h-10 border-sky-500 border rounded-md mb-5'
+            name="name" 
+            onChange={(e) => setForm({...form, name: e.target.value})}
+            placeholder="Sample-Name"
+            type="string"
+            data-testid="form-input-name"
+            />
+
             <p className='w-80 h-10 text-2xl mx-5 text-sky-300'>Fire-Rate</p>
 
             <input 
@@ -136,9 +149,9 @@ const Home = () => {
 
               <div className='flex flex-col'>
               {
-               posts.map(({fr,d,hp,dr,ATE,TTE}) => (
+               posts.map(({n,fr,d,hp,dr,ATE,TTE}) => (
                 <li className='flex grid grid-cols-2 text-sky-700 bg-sky-100 w-80 border rounded-md mt-2 mb-2' key={TTE}>
-                  <div>Name: Sample-Item</div>
+                  <div>Name: {n}</div>
                   <div></div>
                   <div>Fire-Rate: {fr}</div>
                   <div className='ml-2'>Damage: {d}</div>
