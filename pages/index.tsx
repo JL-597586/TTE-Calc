e40cb4f11fdac6c9c6a46f745d80e9f114d8c9a3
Jose Lugo <lugojos004@gmail.com>
2023-07-06 00:47:20 -0500

    change to desktop-friendly layout/ add delete card function

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,6 +60,7 @@ const Home = () => {
         id: uniqueID,
       },
     ]);
+    
   }
 
   AmmoToElim = (hpFloat * (1 + drFloat * 0.01)) / dFloat;
@@ -78,19 +79,38 @@ const Home = () => {
     });
   }
 
+  function removeItem(id:number){
+    const newPosts = posts.filter((item) => item.id !== id)
+    savePosts(newPosts)
+  }
+
+  /*
+  <div className="fixed min-w-screen">
+          <div className="mt-96"></div>
+        </div>
+<button
+            onClick={ScrollToTop}
+            className="bg-sky-900 h-10 w-8 text-2xl text-center text-sky-300 rounded-md active:text-sky-200 active:bg-sky-500"
+          >
+            ↑
+          </button>
+  */
+
   const DisplayList = () => {
     posts.sort((a, b) => (a.TTE > b.TTE ? 1 : -1));
 
-    console.log("posts", posts);
+    //console.log("posts", posts);
     return (
       <>
         {posts.map(({ n, fr, d, hp, dr, ATE, TTE, id }) => (
           <li
-            className="grid grid-cols-2 text-sky-700 bg-sky-100 w-80 border rounded-md mt-2 mb-2"
+            className="grid grid-cols-2 text-sky-700 bg-sky-100 w-70 border rounded-md mt-2 mb-2"
             key={id}
           >
-            <div>Name: {n}</div>
-            <div></div>
+            <div className="bg-sky-600 w-6 text-sky-200 rounded-md text-center text-sm font-semibold">
+              <button onClick={() => removeItem(id)}>x</button>
+            </div>
+            <div className=" bg-sky-700 text-sky-100 rounded-md w-70">Name: {n}</div>
             <div>Fire-Rate: {fr}</div>
             <div className="ml-2">Damage: {d}</div>
             <div className="">Health-Points: {hp}</div>
@@ -99,6 +119,7 @@ const Home = () => {
             <div className="invisible h-2"></div>
             <div>Time-To-Elim: {TTE.toFixed(3)}s</div>
             <div className="invisible h-2"></div>
+            
           </li>
         ))}
       </>
@@ -112,7 +133,8 @@ const Home = () => {
       </Head>
 
       <main>
-        <h1 className=" flex min-w-screen justify-center items-center text-5xl font-bold text-sky-400 m-2">
+        <div className=" flex bg-neutral-800 min-w-screen">
+        <h1 className=" flex min-w-screen justify-left items-left text-5xl font-bold text-sky-400 m-3">
           Elim-Calc
           <div className="flex w-4"></div>
           <div className="w-10 h-14 bg-sky-400 border rounded-md justify-center grid grid-col-1">
@@ -120,27 +142,21 @@ const Home = () => {
             <div className="flex w-7 h-6 bg-sky-200 border rounded-sm m-1"></div>
           </div>
         </h1>
-
-        <div className="flex min-w-screen justify-center h-10"></div>
-
-        <div className="fixed min-w-screen">
-          <div className="mt-96"></div>
-          <button
-            onClick={ScrollToTop}
-            className="bg-sky-900 h-10 w-8 text-2xl text-center text-sky-300 rounded-md active:text-sky-200 active:bg-sky-500"
-          >
-            ↑
-          </button>
+        <div className="w-6"></div>
+        <div className="mt-2"><Login/></div>
         </div>
 
-        <div className="flex min-w-screen items-center justify-center flex-col">
-          <div><Login/></div>
+        <div className="flex min-w-screen justify-left"></div>
+        
+        <div className="flex min-h-screen">
+        <div className="w-60 mx-8 flex items-left justify-left flex-col">
+          
           <form>
-            <div className="h-8"></div>
-            <p className="w-80 h-10 text-2xl mx-1 text-sky-300">Item-Name</p>
+            <div className="h-2"></div>
+            <p className="h-8 text-xl mx-1 text-sky-300">Item-Name</p>
 
             <input
-              className="w-80 h-10 border-sky-500 border rounded-md mb-5"
+              className="h-8 border-sky-500 border rounded-md mb-2"
               name="name"
               onChange={(e) => setForm({ ...form, name: e.target.value })}
               placeholder="Sample-Item"
@@ -148,10 +164,10 @@ const Home = () => {
               data-testid="form-input-name"
             />
 
-            <p className="w-80 h-10 text-2xl ml-1 text-sky-300">Fire-Rate</p>
+            <p className="h-8 text-xl ml-1 text-sky-300">Fire-Rate</p>
 
             <input
-              className="w-80 h-10 border-sky-500 border rounded-md mb-5"
+              className="h-8 border-sky-500 border rounded-md mb-2"
               name="fireRate"
               onChange={(e) => setForm({ ...form, fireRate: e.target.value })}
               placeholder="4"
@@ -159,10 +175,10 @@ const Home = () => {
               data-testid="form-input-fireRate"
             />
 
-            <p className="w-80 h-10 text-2xl ml-1 text-sky-300">Damage</p>
+            <p className="h-8 text-xl ml-1 text-sky-300">Damage</p>
 
             <input
-              className="w-80 h-10 border-sky-500 border rounded-md mb-5"
+              className="h-8 border-sky-500 border rounded-md mb-2"
               name="damage"
               onChange={(e) => setForm({ ...form, damage: e.target.value })}
               placeholder="25"
@@ -170,12 +186,12 @@ const Home = () => {
               data-testid="form-input-damage"
             />
 
-            <p className="w-80 h-10 text-2xl ml-1 text-sky-300">
+            <p className="h-8 text-xl ml-1 text-sky-300">
               Health-Points
             </p>
 
             <input
-              className="w-80 h-10 border-sky-500 border rounded-md mb-5"
+              className="h-8 border-sky-500 border rounded-md mb-2"
               name="healthPoints"
               onChange={(e) =>
                 setForm({ ...form, healthPoints: e.target.value })
@@ -185,12 +201,12 @@ const Home = () => {
               data-testid="form-input-healthPoints"
             />
 
-            <p className="w-80 h-10 text-2xl ml-1 text-sky-300">
+            <p className="h-8 text-xl ml-1 text-sky-300">
               Damage-Reduction %
             </p>
 
             <input
-              className="w-80 h-10 border-sky-500 border rounded-md mb-5"
+              className="h-8 border-sky-500 border rounded-md mb-2"
               name="damageReduction"
               onChange={(e) =>
                 setForm({ ...form, damageReduction: e.target.value })
@@ -203,29 +219,33 @@ const Home = () => {
             <button className='w-96 h-10 border-sky-500 border rounded-md mb-5 text-2xl text-sky-300'>Submit</button>
           */}
 
-            <div className="flex w-80 h-12 border-sky-500 border rounded-md mb-5 text-2xl text-sky-300 items-center">
+            <div className="flex h-10 border-sky-500 border rounded-md mb-2 text-xl text-sky-300 items-center">
               <div className="ml-2">Projectile-To-Elim:</div>
               <div className="ml-2 text-sky-200">{AmmoToElimCeiling}</div>
             </div>
-            <div className="flex w-80 h-12 border-sky-500 border rounded-md mb-5 text-2xl text-sky-300 items-center">
+            <div className="flex h-10 border-sky-500 border rounded-md mb-2 text-xl text-sky-300 items-center">
               <div className="ml-2">Time-To-Elim (s):</div>
               <div className="ml-2 text-sky-200">{TimeToElim.toFixed(3)}</div>
             </div>
             <div>
               <button
+                id="newCard"
                 type="button"
                 onClick={SaveForm}
-                className="flex justify-center items-center w-80 h-12 border-sky-500 border rounded-md bg-sky-600 mb-5 text-3xl text-sky-100 active:bg-sky-500"
-              >
+                className="flex justify-center items-center h-12 border-sky-500 border rounded-md bg-sky-600 mb-5 text-2xl text-sky-100 active:bg-sky-500 p-2">
                 Save Item
               </button>
             </div>
 
-            <div className="flex flex-col">
-              <DisplayList />
-            </div>
+            
           </form>
         </div>
+        <div className=""></div>
+        <div className="h-screen flex flex-col overflow-y-scroll">
+              <DisplayList />
+        </div>
+        </div>
+        
       </main>
 
       <footer></footer>
--- pages/login.jsx
+++ pages/login.jsx
@@ -7,17 +7,17 @@ const Login = () => {
 
     if (session) {
         return (
-        <div className="w-80 h-10 text-l mx-1 text-sky-200">
-            <p>Welcome, {session.user.name}</p>
-            <button className="w-80 h-8 border-sky-500 border rounded-md" onClick={()=>signOut()}>Sign out</button>
+        <div className="w-60 h-10 text-l text-sky-200">
+            <p className="mx-1">Welcome, {session.user.name}</p>
+            <button className="w-60 h-8 border-sky-500 border rounded-md" onClick={()=>signOut()}>Sign out</button>
         </div>
         )
     }
     else {
         return (
-            <div  className="w-80 h-10 text-l mx-1 text-sky-200">
-                <p>You are not signed in</p>
-                <button className="w-80 h-8 border-sky-500 border rounded-md" onClick={()=>signIn()}>Sign in</button>
+            <div  className="w-60 h-10 text-l text-sky-200">
+                <p className="mx-1">You are not signed in</p>
+                <button className="w-60 h-8 border-sky-500 border rounded-md" onClick={()=>signIn()}>Sign in</button>
             </div>
         )
     }
